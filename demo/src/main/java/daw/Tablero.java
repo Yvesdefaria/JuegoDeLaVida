6ba2aa9dfb3cfc61abe108b7557582f3f629396e
yvesdefaria <yvesdefaria@gmail.com>
2025-03-27 11:04:45 +0100

    Tablerov3

--- demo/src/main/java/daw/Tablero.java
+++ demo/src/main/java/daw/Tablero.java
@@ -1,28 +1,28 @@
 package daw;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class Tablero {
-
 
     public static int getMAX_SIZE() {
         return MAX_SIZE;
     }
+
     private Celula[][] matriz;
     private int N;
     private static final int MAX_SIZE = 25;
 
-
-    //Constructor de la clase Tablero
+    // Constructor de la clase Tablero
     public Tablero(int N) {
         this.matriz = new Celula[N][N];
-        this.N = N;
-
 
         // Iniciamos la matriz de células
+        if (N <= MAX_SIZE && N > 0) {
+            this.N = N;
+        } else {
+            this.N = 25;
+        }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 matriz[i][j] = new Celula(false);
@@ -30,32 +30,28 @@ public class Tablero {
         }
     }
 
-
     public Celula[][] getMatriz(int fila, int columna) {
         return matriz;
     }
-
 
     public int getN() {
         return N;
     }
 
-
     public static int getMaxSize() {
         return MAX_SIZE;
     }
 
-
     // public static int[][] crearMatriz(int filas) {
-    //   int[][] matriz = new int[filas][filas];
+    // int[][] matriz = new int[filas][filas];
     // for (int i = 0; i < matriz.length; i++) {
-    //   for (int j = 0; j < matriz[i].length; j++) {
-    //      matriz[i][j] = 0;
-    //  }
-    //}
-    //   return matriz;
+    // for (int j = 0; j < matriz[i].length; j++) {
+    // matriz[i][j] = 0;
     // }
-// Método para mostrar el estado actual del tablero
+    // }
+    // return matriz;
+    // }
+    // Método para mostrar el estado actual del tablero
     public static void mostrarMatriz(Celula[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
@@ -64,12 +60,11 @@ public class Tablero {
                 } else {
                     System.out.print("O ");
                 }
-                
+
             }
             System.out.println();
         }
     }
-
 
     // Método que calcula la cantidad inicial de células vivas según un porcentaje
     public static int calcularVivasIniciales(int N) {
@@ -77,37 +72,31 @@ public class Tablero {
         System.out.println("Elija el porcentaje inicial de células vivas (0-100):");
         double porcentaje = teclado.nextDouble();
 
-
         // Validar que el porcentaje esté en el rango permitido
         while (porcentaje < 0 || porcentaje > 100) {
             System.out.println("Porcentaje inválido. Introduzca un valor entre 0 y 100:");
             porcentaje = teclado.nextDouble();
         }
 
-
         double numCasillas = N * N;
         return (int) ((porcentaje * numCasillas) / 100.0);
     }
 
-
-    //Metodo para colocar las celulas en el tablero de forma aleatoria
+    // Metodo para colocar las celulas en el tablero de forma aleatoria
     public static void colocacionRandom(Celula[][] matriz, int porcentaje) {
         Random r = new Random();
         int fila;
         int columna;
-        do{
-        
-           
+        do {
+
             fila = r.nextInt(matriz.length);
             columna = r.nextInt(matriz.length);
             matriz[fila][columna].setEstaVivo(true);
-        
-        
-    }while(contarVivas(matriz) != porcentaje);
-        
-    }
-//Metodo para colocar las celulas en el tablero elijiendo la casilla
 
+        } while (contarVivas(matriz) != porcentaje);
+
+    }
+    // Metodo para colocar las celulas en el tablero elijiendo la casilla
 
     public static void colocacionManual(Celula[][] matriz, int porcentaje) {
         Scanner teclado = new Scanner(System.in);
@@ -118,12 +107,11 @@ public class Tablero {
             fila = teclado.nextInt();
             System.out.println("Indique la columna(1-N)");
             columna = teclado.nextInt();
-            matriz[fila-1][columna-1].setEstaVivo(true);
+            matriz[fila - 1][columna - 1].setEstaVivo(true);
         }
     }
 
-
-    //Nos devuelve la cantidad de vivas en el tablero
+    // Nos devuelve la cantidad de vivas en el tablero
     public static int contarVivas(Celula[][] matriz) {
         int cont = 0;
         for (int i = 0; i < matriz.length; i++) {
@@ -131,22 +119,51 @@ public class Tablero {
                 if (matriz[i][j].isEstaVivo() == true) {
                     cont++;
 
-
                 }
             }
         }
         return cont;
     }
 
-
-    //Metodo que nos dice si una celula esta viva o muerta
+    // Metodo que nos dice si una celula esta viva o muerta
     public static boolean comprobrarEstado(int fila, int columna, Celula[][] matriz) {
-        return (matriz[fila][columna].isEstaVivo() );
+        return (matriz[fila][columna].isEstaVivo());
     }
-
 
     public Celula[][] getMatriz() {
         return matriz;
     }
-}
 
+    // metodo para comprobar si la posicion no esta fuera de la matriz
+    public static boolean posicionValida(int fila, int columna, Celula[][] matriz) {
+        return fila >= 0 && fila < matriz.length && columna >= 0 && columna < matriz[fila].length;
+    }
+
+    // metodo la imprimir las casillas de alrededor
+    public static int contarVecina(int fila, int columna, Celula[][] matriz) {
+        boolean estado;
+        int cont = 0;
+        // recorro la fila anterio, lasuya y las posterior
+
+        for (int i = fila - 1; i <= fila + 1; i++) {
+            // recorro la columna anterio, lasuya y las posterior
+            for (int j = columna - 1; j <= columna + 1; j++) {
+                if (posicionValida(i, j, matriz)) {
+
+                    estado = comprobrarEstado(i, j, matriz);
+                    if (estado) {
+                        cont++;
+                    }
+                }
+
+            }
+
+        }
+        return cont;
+
+    }
+
+    public static void update(Celula[][] matriz){
+        int numeroCellViva = contarVecina(MAX_SIZE, MAX_SIZE, matriz)
+    }
+}
